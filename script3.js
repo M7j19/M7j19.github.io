ee50a0de8001a8ed7c5b3f1658380ee0cbda54b7
M7j19 <145953689+M7j19@users.noreply.github.com>
2023-11-19 02:51:51 -0800

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,6 +1,8 @@
   document.addEventListener("DOMContentLoaded", function () {
             var images = [
-                "phone.jpg",
+                "img/nothing.jpg",
+          "img/Macbook_air.jpg",
+             "img/CanonS10.jpg",
                 // Add more image URLs as needed
             ];
             var index = 0;
